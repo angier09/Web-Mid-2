746ad289f0d246f985cb3530aeb98d9c23a9438d
angier09 <hateemkhurram199@gmail.com>
2024-03-13 13:44:19 +0500

    q1 js

---
+++ q1.js
@@ -0,0 +1,23 @@
+// This is a one-line JavaScript comment
+
+/* This is another one-line JavaScript comment */
+
+/*
+This is a multi-line
+JavaScript comment
+because it spans
+multiple lines
+*/
+
+/* We commented the function statement below */
+/*
+function sayHello() {
+  console.log('Hello there!!!')
+}
+*/
+
+/* Below is an uncommented JavaScript statement */
+function sayHi()
+{
+    console.log("Hi there!!!");
+}
